7f4920ab331b4055cfbb508c018a782d360496f7
hiepbanguyen <nguyenbahiep001@gmail.com>
2022-11-23 01:31:02 +0700

    add localstorage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import CreateNoteCard from "./components/CreateNoteCard";
 import CardGrid from "./components/CardGrid";
 import PrimarySearchAppBar from "./components/AppBar.js";
 
 function App() {
-  const data = [
-    { id: 1, title: 'Title 1 Example', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-    { id: 2, title: 'Title 2 Example', content: 'Lorem ipsum dolo commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-    { id: 3, title: 'Title 3 Example', content: 'Lorem ipsum dolo commodo consequat. Duis aute irure dolor.' },
-  ]
-  const [notes, setNotes] = useState(data);
+  const data = JSON.parse(localStorage.getItem('notes'));
+  const [notes, setNotes] = useState(data ?? []);
 
   const addNote = (note) => {
-    const newNote = { ...note, id: notes.length + 1}
-    setNotes(notes.concat(newNote));
+    const largestNoteId = Number(localStorage.getItem('largestNoteId'));
+    // console.log(JSON.stringify(largestNoteId));
+    const newNote = { ...note, id: largestNoteId + 1};
+    // console.log(JSON.stringify(newNote));
+    localStorage.setItem('largestNoteId', newNote.id);
+    const newNotes = [...notes, newNote];
+    updateStateAndLocalStorage(newNotes);
   }
 
   const deleteNote = (id) => {
-    setNotes(notes.filter((note) => note.id !== id));
+    const newNotes = notes.filter((note) => note.id !== id);
+    updateStateAndLocalStorage(newNotes);
   }
-  //
+
   const editNote = (id, editedNote) => {
-    // setEditing(false);
-    setNotes(notes.map((note) => (note.id === id ? editedNote : note)))
+    const newNotes = notes.map((note) => (note.id === id ? editedNote : note));
+    updateStateAndLocalStorage(newNotes);
+  }
+
+  const updateStateAndLocalStorage = (newNotes) => {
+    localStorage.setItem('notes', JSON.stringify(newNotes));
+    setNotes(newNotes);
   }
 
   const searchNotes = (keyword) => {
-    if (keyword) {
+    if (keyword.length > 0) {
+
       const notesContainKeyword = [];
       for (const i of data) {
         const pool = i.content.concat(i.title).toLowerCase();
@@ -37,15 +45,6 @@ function App() {
       setNotes(notesContainKeyword);
     }
   }
-  //
-  // const [editing, setEditing] = useState(false)
-  // const initialCurrentUser = { id: null, name: '', username: '' }
-  // const [currentUser, setCurrentUser] = useState(initialCurrentUser)
-  //
-  // const editRow = (user) => {
-  //   setEditing(true)
-  //   setCurrentUser({ id: user.id, name: user.name, username: user.username })
-  // }
 
   return (
       <div className="container">
