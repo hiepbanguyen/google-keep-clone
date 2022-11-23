e0b3ac7e402c3edacca7f9d6cd71e5f1388627eb
hiepbanguyen <nguyenbahiep001@gmail.com>
2022-11-23 12:30:21 +0700

    add blank state

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,31 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import NoteCard from "./NoteCard";
-import {Box, Grid} from "@mui/material";
+import {Box, Grid, Typography} from "@mui/material";
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 
 const CardGrid = (props) => {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<Grid container spacing={3}>
 				{props.notes.length > 0 ? (
-					props.notes.map(note => (
+					<Grid container spacing={3}>
+						{props.notes.map(note => (
 						<Grid item xs>
 							<NoteCard key={note.id} note={note} deleteNote={props.deleteNote} editNote={props.editNote}/>
 						</Grid>
-						// <div key={note.id} className="flex-small">
-						// 	<span>{note.title}</span>
-						// 	<p>{note.content}</p>
-						// 	<button
-						// 		className="button muted-button"
-						// 		onClick={() => props.deleteNote(note.id)}
-						// 	>
-						// 		Delete
-						// 	</button>
-						// </div>
-					))
+						))}
+					</Grid>
 				) : (
-					<div></div>
+					<div>
+						<LightbulbOutlinedIcon sx={{color: "#E5E5E5", mx: "49%", mt: 10, transform: "scale(5,5)"}}/>
+						<Typography sx={{color: "#727272", fontSize: 25, width: "fit-content", mx: "auto", mt: 5}}>Notes you add appear hear</Typography>
+					</div>
 				)}
-			</Grid>
 		</Box>
 	)
 }
